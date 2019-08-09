204af1d54456ee0537e0e5803e75290d1f17ff38
Vlad Ivanov <vivanov@metaprime.ru>
2019-08-09 17:50:59 +0300

    lint fixes

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -4,26 +4,26 @@ import store from '@/store/index';
 register('/service-worker.js', {
   registrationOptions: { scope: './' },
   ready (/*registration*/) {
-    console.log('Service worker is active.')
+    //console.log('Service worker is active.')
   },
   registered (/*registration*/) {
-    console.log('Service worker has been registered.')
+    //console.log('Service worker has been registered.')
   },
   cached (/*registration*/) {
-    console.log('Content has been cached for offline use.')
+    //console.log('Content has been cached for offline use.')
   },
   updatefound (/*registration*/) {
-    console.log('New content is downloading.')
+    //console.log('New content is downloading.')
   },
   updated (/*registration*/) {
-    console.log('New content is available; please refresh.');
+    //console.log('New content is available; please refresh.');
     store.dispatch('app/snackOpen', 'Новый контент. Пожалуста, обновите страницу');
   },
   offline () {
-    console.log('No internet connection found. App is running in offline mode.');
+    //console.log('No internet connection found. App is running in offline mode.');
     store.dispatch('app/snackOpen', 'Переход в оффлайн-режим');
   },
   error (error) {
-    console.error('Error during service worker registration:', error)
+    //console.error('Error during service worker registration:', error)
   }
 })
